cf13876cd8ee57a25ea32fed6f64253cc0ed10c9
Masao-Someki <masao.someki@gmail.com>
2025-03-28 04:13:09 -0400

    Add config-save function to run inference with ESPnet's inference API

--- egs3/train.py
+++ egs3/train.py
@@ -82,6 +82,14 @@ if __name__ == "__main__":
 
     print("Config loaded", flush=True)
 
+    # save the configuration for inference
+    ez.save_espnet_config("asr", config, os.path.join(config.expdir, "config.yaml"))
+
+    # Set random seed if required
+    if getattr(config, "seed", None) is not None:
+        assert isinstance(config.seed, int), "seed should be an integer"
+        L.seed_everything(config.seed)
+
     # Set parallelism config
     set_parallel(config.parallel)
     print("Parallelism config set", flush=True)
